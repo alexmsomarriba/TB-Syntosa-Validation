6f21964ef550a2ed07cf3d459954f4e2b5e5a018
Alex Somarriba <alex@teambond.io>
2020-07-15 19:34:59 -0500

    inital commit of validation library.

---
+++ TeamBond.Syntosa.Validation/Libraries/TeamBond.Syntosa.Validation.Core/CodeWriter/ICodeWriter.cs
@@ -0,0 +1,98 @@
+﻿namespace TeamBond.Syntosa.Validation.Core.CodeWriter
+{
+    using System.IO;
+
+    /// <summary>
+    /// Defines methods for .
+    /// </summary>
+    public interface ICodeWriter
+    {
+        /// <summary>
+        /// Gets the file extension.
+        /// </summary>
+        string FileExtension { get; }
+
+        /// <summary>
+        /// Gets the display name.
+        /// </summary>
+        string DisplayName { get; }
+
+        /// <summary>
+        /// The get type name.
+        /// </summary>
+        /// <param name="type">
+        /// The type.
+        /// </param>
+        /// <param name="config">
+        /// The config.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        string GetTypeName(JsonType type, IJsonClassGeneratorConfig config);
+
+        /// <summary>
+        /// Writes the class.
+        /// </summary>
+        /// <param name="config">
+        /// The config.
+        /// </param>
+        /// <param name="stringWriter">
+        /// The string writer.
+        /// </param>
+        /// <param name="type">
+        /// The type.
+        /// </param>
+        void WriteClass(IJsonClassGeneratorConfig config, TextWriter stringWriter, JsonType type);
+
+        /// <summary>
+        /// Writes the file start.
+        /// </summary>
+        /// <param name="config">
+        /// The config.
+        /// </param>
+        /// <param name="stringWriter">
+        /// The string writer.
+        /// </param>
+        void WriteFileStart(IJsonClassGeneratorConfig config, TextWriter stringWriter);
+
+        /// <summary>
+        /// Writes the file end.
+        /// </summary>
+        /// <param name="config">
+        /// The config.
+        /// </param>
+        /// <param name="stringWriter">
+        /// The string writer.
+        /// </param>
+        void WriteFileEnd(IJsonClassGeneratorConfig config, TextWriter stringWriter);
+
+        /// <summary>
+        /// Writes the namespace start.
+        /// </summary>
+        /// <param name="config">
+        /// The config.
+        /// </param>
+        /// <param name="stringWriter">
+        /// The string writer.
+        /// </param>
+        /// <param name="root">
+        /// The root.
+        /// </param>
+        void WriteNamespaceStart(IJsonClassGeneratorConfig config, TextWriter stringWriter, bool root);
+
+        /// <summary>
+        /// Writes the namespace end.
+        /// </summary>
+        /// <param name="config">
+        /// The config.
+        /// </param>
+        /// <param name="stringWriter">
+        /// The string writer.
+        /// </param>
+        /// <param name="root">
+        /// The root.
+        /// </param>
+        void WriteNamespaceEnd(IJsonClassGeneratorConfig config, TextWriter stringWriter, bool root);
+    }
+}
---
+++ TeamBond.Syntosa.Validation/Libraries/TeamBond.Syntosa.Validation.Core/CodeWriter/SupportedJsonTypes.cs
@@ -0,0 +1,93 @@
+﻿namespace TeamBond.Syntosa.Validation.Core.CodeWriter
+{
+    /// <summary>
+    /// The supported json types.
+    /// </summary>
+    public enum SupportedJsonTypes
+    {
+        /// <summary>
+        /// The anything.
+        /// </summary>
+        Anything,
+
+        /// <summary>
+        /// The string.
+        /// </summary>
+        String,
+
+        /// <summary>
+        /// The boolean.
+        /// </summary>
+        Boolean,
+
+        /// <summary>
+        /// The integer.
+        /// </summary>
+        Integer,
+
+        /// <summary>
+        /// The long.
+        /// </summary>
+        Long,
+
+        /// <summary>
+        /// The float.
+        /// </summary>
+        Float,
+
+        /// <summary>
+        /// The date.
+        /// </summary>
+        Date,
+
+        /// <summary>
+        /// The nullable integer.
+        /// </summary>
+        NullableInteger,
+
+        /// <summary>
+        /// The nullable long.
+        /// </summary>
+        NullableLong,
+
+        /// <summary>
+        /// The nullable float.
+        /// </summary>
+        NullableFloat,
+
+        /// <summary>
+        /// The nullable boolean.
+        /// </summary>
+        NullableBoolean,
+
+        /// <summary>
+        /// The nullable date.
+        /// </summary>
+        NullableDate,
+
+        /// <summary>
+        /// The object.
+        /// </summary>
+        Object,
+
+        /// <summary>
+        /// The array.
+        /// </summary>
+        Array,
+
+        /// <summary>
+        /// The dictionary.
+        /// </summary>
+        Dictionary,
+
+        /// <summary>
+        /// The nullable something.
+        /// </summary>
+        NullableSomething,
+
+        /// <summary>
+        /// The non constrained.
+        /// </summary>
+        NonConstrained
+    }
+}
