5898d59d9c6327fa5c964ae9c289feae9474fc99
Alex Somarriba <alex@teambond.io>
2020-07-26 19:51:48 -0500

    integated new TeamBond.Engine packages

---
+++ TeamBond.Syntosa.Validation/Tools/TeamBond.Syntosa.Validation.JsonToSyntosa/TeamBondEngineJsonToSyntosaBase.cs
@@ -0,0 +1,11 @@
+﻿namespace TeamBond.Syntosa.Validation.JsonToSyntosa
+{
+    using TeamBond.Application.Framework;
+
+    /// <summary>
+    /// The team bond engine json to syntosa base.
+    /// </summary>
+    public abstract class TeamBondEngineJsonToSyntosaBase : TeamBondEngineApplicationBase
+    {
+    }
+}
